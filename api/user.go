3fa05a7f5ef8b8d6266d8315632ab49d65f88554
Gabriel Handford <gabrielh@gmail.com>
2021-05-24 17:52:52 -0700

    api: User.Name

--- api/user.go
+++ api/user.go
@@ -3,10 +3,15 @@ package api
 import "github.com/keys-pub/keys"
 
 type User struct {
-	KID      keys.ID `json:"kid"`
-	Username string  `json:"username"`
+	KID  keys.ID `json:"kid"`
+	Name string  `json:"name"`
 }
 
 type UsersLookupRequest struct {
 	KIDs []keys.ID `json:"kids"`
+}
+
+type UsersResponse struct {
+	Users []*User `json:"users"`
+	Index int64   `json:"idx"`
 }
